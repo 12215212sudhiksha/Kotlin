0a8aaf43449ed62c1c6fbb31b535749975e1657e
TSV Sudhiksha <119122300+12215212sudhiksha@users.noreply.github.com>
2024-08-28 01:06:23 +0530

    26th Aug to 27th Aug.kt

--- 26th Aug to 27th Aug.kt
+++ 26th Aug to 27th Aug.kt
@@ -87,4 +87,48 @@ fun main(){
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
 //27th Aug
+fun main() {
+    var i = 5
+    while(i>0){
+        println(i)
+        i--
+    }
+}
+
+fun main() {
+    var i = 5
+    do{
+        println("value is: $i")
+        i--
+    }
+    while(i>5)
+}
+
+
+fun main() {
+    var i = 5
+    do{
+        println("value is: $i")
+        i--
+    }
+    while(i>0)
+}
+
+fun main() {
+    var i = 5
+    do{
+        println("value is: $i")
+        i--
+    }
+    while(i>=0)
+}
+
+fun main() {
+    var i = 1
+    do{
+        println("value is: $i")
+        i++
+    }
+    while(i<=5)
+}
 
